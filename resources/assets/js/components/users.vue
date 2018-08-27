648ca087e646daf8575d070f3bc9c32434734faf
Carlos Henrique Reis <carlos_henreis@unifei.edu.br>
2018-08-26 21:58:12 -0300

    fix bug order users array

--- resources/assets/js/components/users.vue
+++ resources/assets/js/components/users.vue
@@ -1,47 +1,33 @@
+<template>
+  <div>
+    <table class="table table-bordered table-striped table-hover">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="u in list" :key="u.name">
+          <th>{{ u.name }}</th>
+          <th>{{ u.email }}</th>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
 <script>
 export default {
   props: ["users"],
 
-  data() {
+  data () {
     return {
       list: []
-    };
-  },
-
-  mounted() {
-    this.list = JSON.parse(this.users);
-  },
-
-  computed: {
-    order: function() {
-      return _.orderBy(this.list, "name");
     }
   },
 
-  methods: {
-   
+  mounted() {
+    this.list = JSON.parse(this.users).sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} )
   }
-};
+}
 </script>
-
-<template>
-   <div>
-        <table class="table table-bordered table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="u in order" :key="u.name">
-                    <th>{{ u.name }}</th>
-                    <th>{{ u.email }}</th>
-                </tr>
-              
-            </tbody>
-        </table> 
-   </div>
-</template>
-
-
